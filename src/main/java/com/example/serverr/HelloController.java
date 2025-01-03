e7d745c7674fab3596bc3e8985b5ff3f9ac0cc5f
SimoSpark <mohamedoukhajou@gmail.com>
2025-01-03 09:40:52 +0100

    Sending-Messages-=

--- src/main/java/com/example/serverr/HelloController.java
+++ src/main/java/com/example/serverr/HelloController.java
@@ -55,6 +55,49 @@ public class HelloController implements Initializable {
             String messageToSend = tf_message.getText();
             if (!messageToSend.isEmpty() && server != null) {
                 sendMessage(messageToSend);
-
-
             }
+            //HNA FIN 7BSST
+        });
+    }
+
+    private void sendMessage(String messageToSend) {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+
+        Text text = new Text(messageToSend);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle(
+                "-fx-color: rgb(239, 242, 255);" +
+                        "-fx-background-color: rgb(15, 125, 242);" +
+                        "-fx-background-radius: 20px;");
+
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        text.setFill(Color.color(0.934, 0.925, 0.996));
+
+        hBox.getChildren().add(textFlow);
+        vbox_messages.getChildren().add(hBox);
+
+        server.sendMessageToClient(messageToSend);
+        tf_message.clear();
+    }
+
+    public static void addLabel(String messageFromClient, VBox vBox) {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+
+        Text text = new Text(messageFromClient);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle(
+                "-fx-background-color: rgb(233, 233, 235);" +
+                        "-fx-background-radius: 20px;");
+
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        hBox.getChildren().add(textFlow);
+
+        Platform.runLater(() -> vBox.getChildren().add(hBox));
+    }
+}
